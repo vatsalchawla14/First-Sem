0009594db7ff2d9008fa03b81d40b001c7bf1ebf
Vatsal Chawla <vatsalchawla9@gmail.com>
2024-12-01 12:18:55 +0530

    6.c

---
+++ Assignment 1/6.c
@@ -0,0 +1,19 @@
+//Q6. WAP to accept a coordinate point in an XY coordinate system and determine in which quadrant the coordinate point lies.
+
+
+#include <stdio.h>
+int main() 
+{
+int x = 7, y = 9;
+if (x > 0 && y > 0)
+printf("The coordinate point (%d,%d) lies in the First quadrant.\n", x, y);
+else if (x < 0 && y > 0)
+printf("The coordinate point (%d,%d) lies in the Second quadrant.\n", x, y);
+else if (x < 0 && y < 0)
+printf("The coordinate point (%d,%d) lies in the Third quadrant.\n", x, y);
+else if (x > 0 && y < 0)
+printf("The coordinate point (%d,%d) lies in the Fourth quadrant.\n", x, y);
+else
+printf("The coordinate point (%d,%d) lies on the origin or an axis.\n", x, y);
+return 0;
+}
