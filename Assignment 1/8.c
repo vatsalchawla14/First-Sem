6a409970c3cbcbdf695bf2a6857e17b62edf4c12
Vatsal Chawla <vatsalchawla9@gmail.com>
2024-12-01 12:19:30 +0530

    8.c

---
+++ Assignment 1/8.c
@@ -0,0 +1,16 @@
+//Q8. WAP to print below mentioned pattern:
+
+#include <stdio.h>
+
+int main()
+{ 
+    int n = 5;
+for (int i = 1; i <= n; i++) { 
+    for (int j = 1; j <= i; j++) {
+if ((i + j) % 2 == 0) 
+printf("1");
+else printf("0");
+}
+printf("\n"); }
+return 0; 
+}
