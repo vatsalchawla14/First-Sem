54f42426104a705535b05cac98353d72bf80ef4f
Vatsal Chawla <vatsalchawla9@gmail.com>
2024-12-01 12:18:38 +0530

    5.c

---
+++ Assignment 1/5.c
@@ -0,0 +1,15 @@
+//Q5. WAP to check whether number is Perfect Number or not.
+
+
+#include <stdio.h>
+int main()
+{
+int num = 28, sum = 0, i;
+for (i = 1; i < num; i++) {
+if (num % i == 0) sum += i;
+}
+if (sum == num) printf("Perfect Number\n");
+else
+printf("Not a Perfect Number\n");
+return 0;
+}
