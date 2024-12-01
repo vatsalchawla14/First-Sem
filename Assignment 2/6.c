3a91df50908a6d841dcbd551119ff5e20c542a98
Vatsal Chawla <vatsalchawla9@gmail.com>
2024-12-01 12:23:36 +0530

    6.c

---
+++ Assignment 2/6.c
@@ -0,0 +1,8 @@
+//Q6. WAP to find average score of the Marks array.
+#include <stdio.h>
+int main() {
+int marks[] = {78, 99, 55, 99, 39}; int n = 5, sum = 0;
+for (int i = 0; i < n; i++) { sum += marks[i];
+}
+float average = sum / (float)n; printf("Average score: %.2f", average);
+return 0; }
